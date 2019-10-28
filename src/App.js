ee1b44628905a029e2b5863bf7c00dc4e8d66676
Atheer M <43179208+atheer10@users.noreply.github.com>
2019-10-28 22:34:35 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+import "./App.css";
+import Student from "./Student";
+import students from "./Students";
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>All Students </h1>
+        <ul>
+          {students.map(studentName => (
+            // console.log(studentName)
+            <Student name={studentName} />
+          ))}
+        </ul>
+      </div>
+    );
+  }
 }
-
-export default App;
